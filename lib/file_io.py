972ef41ab3978d109c161cce67aa7c7d41f26d25
Lydia Nyamoita <lydianyamo18@outlook.com>
2023-08-28 17:34:02 +0300

    Done

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,12 +1,21 @@
 def write_file(file_name, file_content):
     
     with open("file_name.txt", mode ="w",encoding="utf-8") as file_name:
-        file_name.write(file_content)
+       return file_name.write(file_content)
 def append_file(file_name, append_content):
     with open("file_name.txt", mode="w", encoding="utf-8") as file_name:
-        file_name.write(append_content)
+        return file_name.write(append_content)
 
 def read_file(file_name):
     with open("file_name.txt", encoding="utf-8") as file_name:
         for line in file_name:
-            print(line)
+            return(line)
+
+
+# use write_file function. 
+print(write_file(file_name="logfile", file_content="Log 1: 5 bananas added" ))
+print(append_file(file_name="logfile", append_content="Log 2: 3 bananas subtracted"))
+
+print (read_file(file_name="logfile"))
+# Log 1: 5 bananas added
+# Log 2: 3 bananas subtracted
